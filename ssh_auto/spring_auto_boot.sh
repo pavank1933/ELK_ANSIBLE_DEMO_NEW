4916ab98eebcb672c99584bf48ca4aadcc361d3e
root <root@ip-172-31-42-139.us-east-2.compute.internal>
2018-07-03 19:59:41 +0000

    update

--- ssh_auto/spring_auto_boot.sh
+++ ssh_auto/spring_auto_boot.sh
@@ -7,16 +7,17 @@ yum update -y
 yum install wget -y
 yum install git -y
 cd  /opt
-wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u161-b12/2f38c3b165be4555a1fa6e98c45e0808/jdk-8u161-linux-x64.tar.gz"
-tar xzf jdk-8u161-linux-x64.tar.gz
+wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u171-b11/512cd62ec5174c3487ac17c61aaa89e8/jdk-8u171-linux-x64.tar.gz"
+
+tar xzf jdk-8u171-linux-x64.tar.gz
 rm -rf jdk-8*
-alternatives --install /usr/bin/java java /opt/jdk1.8.0_161/bin/java 2
+alternatives --install /usr/bin/java java /opt/jdk1.8.0_171/bin/java 2
 #opt for 1
 #alternatives --config java
-alternatives --install /usr/bin/jar jar /opt/jdk1.8.0_161/bin/jar 2
-alternatives --install /usr/bin/javac javac /opt/jdk1.8.0_161/bin/javac 2
-alternatives --set jar /opt/jdk1.8.0_161/bin/jar
-alternatives --set javac /opt/jdk1.8.0_161/bin/javac
+alternatives --install /usr/bin/jar jar /opt/jdk1.8.0_171/bin/jar 2
+alternatives --install /usr/bin/javac javac /opt/jdk1.8.0_171/bin/javac 2
+alternatives --set jar /opt/jdk1.8.0_171/bin/jar
+alternatives --set javac /opt/jdk1.8.0_171/bin/javac
 
 
 #Install MAVEN*****
