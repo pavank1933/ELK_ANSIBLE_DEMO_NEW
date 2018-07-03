5af0d997a3eb8e52ea664a7cacd3503c35b417b0
pavan <pavan.venkat33@gmail.com>
2018-07-03 23:55:39 +0530

    update file

--- ssh_auto/hosts_auto.sh
+++ ssh_auto/hosts_auto.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+aws_region="us-east-2"
+
 echo "[elk]" >>../hosts
 
-elk=`aws ec2 describe-instances --region us-east-2 --filters "Name=tag:Name,Values=elk_server" --query "Reservations[*].Instances[*].PublicIpAddress" --output=text`
+elk=`aws ec2 describe-instances --region $aws_region --filters "Name=tag:Name,Values=elk_server" --query "Reservations[*].Instances[*].PublicIpAddress" --output=text`
 
 echo "$elk ansible_ssh_user=ec2-user ansible_ssh_private_key_file=/root/.ssh/id_rsa" >> ../hosts
 
 
 echo "[elk-client]" >> ../hosts
 
-elk_client=`aws ec2 describe-instances --region us-east-2 --filters "Name=tag:Name,Values=elk_client" --query "Reservations[*].Instances[*].PublicIpAddress" --output=text`
+elk_client=`aws ec2 describe-instances --region $aws_region --filters "Name=tag:Name,Values=elk_client" --query "Reservations[*].Instances[*].PublicIpAddress" --output=text`
 
 echo "$elk_client ansible_ssh_user=ec2-user ansible_ssh_private_key_file=/root/.ssh/id_rsa" >> ../hosts
--- ssh_auto/iplist_auto.sh
+++ ssh_auto/iplist_auto.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-echo `aws ec2 describe-instances --region us-east-2 --filters "Name=tag:Name,Values=elk_server" --query "Reservations[*].Instances[*].PublicIpAddress" --output=text` >> iplist
-echo `aws ec2 describe-instances --region us-east-2 --filters "Name=tag:Name,Values=elk_client" --query "Reservations[*].Instances[*].PublicIpAddress" --output=text` >> iplist
+aws_region="us-east-2"
+
+echo `aws ec2 describe-instances --region $aws_region --filters "Name=tag:Name,Values=elk_server" --query "Reservations[*].Instances[*].PublicIpAddress" --output=text` >> iplist
+echo `aws ec2 describe-instances --region $aws_region --filters "Name=tag:Name,Values=elk_client" --query "Reservations[*].Instances[*].PublicIpAddress" --output=text` >> iplist
